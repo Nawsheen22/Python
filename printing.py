60c5108d2029ca8e1f78122c1ee89be4671043ec
Nawsheen22 <nawsheensalsabeel18@gmail.com>
2024-10-17 01:16:30 +0600

    Added print examples to 1.Printing

---
+++ printing.py
@@ -0,0 +1,13 @@
+print ("Hello World!") # You see printf is not used like C 
+
+if 45>30:
+    print("45 is greater than 30")
+
+if 78==89:
+    print("Life is a joke")
+else :
+ print("Error")    
+
+if 6*7:
+   print("Math is fun")
+   print("Math is fun")    #when you print in python , you must know that block space matters
