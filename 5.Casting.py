5dbdffb7f7f9230a0fce3822f133240f0c24f4ca
Nawsheen22 <nawsheensalsabeel18@gmail.com>
2024-10-21 00:09:48 +0600

    Add 5.Casting.py file

---
+++ 5.Casting.py
@@ -0,0 +1,15 @@
+""" Its nothing exceptional , more like type casting like  turning a variable into a specific type"""
+
+
+
+SheWolf=45
+SheWolf1="She is Strong"
+SheWolf2=874387.8900
+
+a=str(SheWolf1)
+b=str(SheWolf2)
+c=float(SheWolf)
+
+print(a,b,c)
+
+print(type(a),type(b),type(c))
