0568dabd5dd0ab7ffb3eb09b3f94c2568aa06c72
Nawsheen22 <nawsheensalsabeel18@gmail.com>
2024-10-21 15:36:26 +0600

    Add 0.User_Input.py file

---
+++ 0.User_Input.py
@@ -0,0 +1,14 @@
+#usercinout 
+inp = int (input ("Enter  & Value:")) #int
+print("The entered value is :",inp)
+
+
+
+inp1 = float(input("Enter & Value:")) #float
+print("The entered value is :",inp1)
+
+
+
+inp2=(input("Enter & Value"))# for String by default it will consider string
+print("The entered value is:",inp2)
+
