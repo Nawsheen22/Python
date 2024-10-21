dac5af7d48f4b0e08bb24b5fdefba8277c348e2f
Nawsheen22 <nawsheensalsabeel18@gmail.com>
2024-10-21 16:25:40 +0600

    Add 9.Format_String.py file

---
+++ 9.Format_String.py
@@ -0,0 +1,18 @@
+#Repeat After me Python is Coolest Language
+#We can add variable value in a String , we also can do some mathmatical operation here 
+a=10
+Addition= f"I am currently doing code for {a} hours"
+
+print(Addition)
+#Mathmatical Operation
+Multiplication=f"I have total {12*4} tk now"
+
+print(Multiplication)
+
+
+#declaring the data type ---> .2f
+price=89
+DataType=f"I am having really good life and i want {price:.2f} billion to help palestine"
+
+print(DataType)
+
