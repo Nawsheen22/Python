68ba2f406672f5ffca84029992bf541a2de5d070
Nawsheen22 <nawsheensalsabeel18@gmail.com>
2024-10-21 15:56:42 +0600

    Add 7.Modify_String.py file

---
+++ 7.Modify_String.py
@@ -0,0 +1,24 @@
+#UpperCase Function  Basically makes everything capital Letter 
+
+
+a="Life is easy when you try to solve problems rather than complaining"
+print(a.upper())
+
+
+#LowerCase Function Basically makes everything small letter
+
+print(a.lower())
+
+#Removes whitespace in "   Nawsheen    " --> "Nawsheen"
+b="    There I was again tonight  "
+print(b.strip()) 
+
+
+#Replace function changes a string or letter to another
+
+print(b.replace("t","Hahaha"))
+
+#Split function basically splits the string into substring
+print(b.split(" "))   #here every space you see counted as index
+print(b.split())
+print(b.split("t"))
