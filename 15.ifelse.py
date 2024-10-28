f97eb3d71a7915accccfebfd489379702c152a24
Nawsheen22 <nawsheensalsabeel18@gmail.com>
2024-10-28 21:56:35 +0600

    Many problem is solved

---
+++ 15.ifelse.py
@@ -0,0 +1,55 @@
+#Write a program whether a person is eligible for voting or not 
+
+
+age=int(input("Enter the age:"))
+print("The age :",age)
+
+if age>=18:
+    print("Eligible for voting")
+else:
+    print("Can not vote")
+
+#Write a program to check whether a number entered by user is even or odd
+
+number = int (input("Enter the number :"))
+print("The number you have entered:",number)
+
+
+
+if number%2==0: #after divided by 2 if the modulus remain 0
+    print("The number is even")
+
+else:
+    print("The number is odd ")
+
+
+#Write a program to see if a number is divisible by 7 or not 
+
+
+Number=int (input("Enter the number:"))
+print("The number you have entered:",Number)
+
+if Number%7==0:
+    print("The number is divisible by 7")
+
+else:
+    print("The number is not divisible by 7")
+
+#Write a program to display "Hello" if a number entered by the user is a multiple of 5 , otherwise bye
+
+Number1=int (input("Enter the number:"))
+print("The number entered by the user:",Number1)
+
+if Number1%5==0:
+    print("Hello")
+
+else:
+    print("Bye")    
+
+
+
+
+
+
+    
+
